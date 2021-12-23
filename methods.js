3a05473d1e9df99891c549bf63c4ebd49451060f
sboteroarango <sboteroarango@gmail.com>
2021-12-23 13:30:34 -0500

    ,

--- methods.js
+++ methods.js
@@ -1,6 +1,12 @@
 // DISCOUNTS
-function calcularValorFinal(){
+function calculateFinalValue(){
     actualPrice = Number(document.getElementById("precioActual").value)
     discount = Number(document.getElementById("porcentajeDescuento").value)
     document.getElementById("respuesta").innerText= "Final Price: " +(actualPrice*(100-discount))/100
+}
+// BMI
+function calculateBMI(){
+    weight = Number(document.getElementById("peso").value)
+    height = Number(document.getElementById("estatura").value)
+    document.getElementById("respuesta").innerText= "Body Mass Index: " +(weight/(height**2)).toFixed(1)
 }
